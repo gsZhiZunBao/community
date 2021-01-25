0b9e3f28223286345abd64014a1b1c13fe3bd701
OldYellow <gsZhiZunBao@163.com>
2021-01-26 00:07:47 +0800

    add github api

--- src/main/java/com/basketball/community/community/controller/AuthorizeController.java
+++ src/main/java/com/basketball/community/community/controller/AuthorizeController.java
@@ -15,17 +15,24 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
     @GetMapping("/callback")
-    public String callback(@RequestParam(name="code") String code, @RequestParam(name = "state") String state) throws IOException {
+    public String callback(@RequestParam(name="code") String code,
+                           @RequestParam(name = "state") String state) throws IOException {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setCode(code);
         accessTokenDTO.setClient_id("1db6cba09e4e72523c67");
-        accessTokenDTO.setClient_secret("8d89c1e4bb0a70f66317e6e1ae7ad0cc083f53c6");
-        accessTokenDTO.setRedirect_uri("http://localhost:8889/callback");
+//        个人token
+//        454a6463f65a4fc6c2ec020401d53ee6c0b7c8b1
+//        应用token
+//        85575d3d1f1319ebd0fc763089941a7208abed05
+        accessTokenDTO.setClient_secret("85575d3d1f1319ebd0fc763089941a7208abed05");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         System.out.println("第二处"+accessToken);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
+        System.out.println("名字"+user.getName());
+        System.out.println(user.getBio());
+        System.out.println(user.getId());
         return "index";
     }
 }
