d16c01dd3cd4f512e921ece64c857882063500b9
OldYellow <gsZhiZunBao@163.com>
2021-02-01 21:02:05 +0800

    add profile page

--- src/main/java/com/basketball/community/community/controller/IndexController.java
+++ src/main/java/com/basketball/community/community/controller/IndexController.java
@@ -1,7 +1,10 @@
 package com.basketball.community.community.controller;
 
+import com.basketball.community.community.dto.PaginationDTO;
+import com.basketball.community.community.dto.QuestionDTO;
 import com.basketball.community.community.mapper.UserMapper;
 import com.basketball.community.community.model.User;
+import com.basketball.community.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,13 +13,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class IndexController {
     @Autowired
     private UserMapper userMapper;
+    @Autowired
+    private QuestionService questionService;
     @GetMapping("/")
-    public String index(HttpServletRequest request){
+    public String index(HttpServletRequest request,
+                        Model model,
+                        @RequestParam(name = "page", defaultValue = "1") Integer page,
+                        @RequestParam(name = "size", defaultValue = "5") Integer size){
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length != 0)
         for (Cookie cookie : cookies) {
@@ -29,6 +38,8 @@ public class IndexController {
                 break;
             }
         }
+        PaginationDTO pagination = questionService.list(page, size);
+        model.addAttribute("pagination", pagination);
         return "index";
     }
 }
